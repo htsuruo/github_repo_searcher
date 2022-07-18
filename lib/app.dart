2d9d67e7be805f71b351884e88c4b5f211f4ae9c
tsuruo <tsuru.dev@gmail.com>
2022-07-18 21:07:33 +0900

    initial setup

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:github_repository_searcher/search_page.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SearchPage(),
+    );
+  }
+}
