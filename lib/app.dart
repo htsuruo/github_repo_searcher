b8e2518b8001af4fec0d7124cbf1d7f9e6f70264
tsuruo <tsuru.dev@gmail.com>
2022-07-18 23:16:47 +0900

    device_previewを導入してみた

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:github_repo_searcher/base_page.dart';
@@ -11,6 +12,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       onGenerateTitle: (context) => context.l10n.appTitle,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:github_repo_searcher/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    DevicePreview(
+      // ignore: avoid_redundant_argument_values
+      enabled: kDebugMode,
+      builder: (context) => const App(),
+    ),
+  );
 }
