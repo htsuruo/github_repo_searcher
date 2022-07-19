287bf15e064e2054c31804c46a7ab43732c362b4
tsuruo <tsuru.dev@gmail.com>
2022-07-19 09:46:57 +0900

    DevicePreviewの利用をmacOSビルド限定にした

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -6,8 +8,10 @@ import 'package:github_repo_searcher/app.dart';
 void main() {
   runApp(
     DevicePreview(
-      // ignore: avoid_redundant_argument_values
-      enabled: kDebugMode,
+      // DevicePreviewはスマートフォンサイズでは設定がしづらいので、
+      // macOSでのビルドのみ使うようにする
+      // iOS/Android通常利用では操作感を確かめる用途もあるのでデバイスプレビューは使わない
+      enabled: !kIsWeb && Platform.isMacOS && kDebugMode,
       builder: (context) => const App(),
     ),
   );
