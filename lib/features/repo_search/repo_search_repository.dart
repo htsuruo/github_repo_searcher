a8b0d633e5df1ca80ec6183a5a87bf7031684129
tsuruo <tsuru.dev@gmail.com>
2022-07-22 13:58:53 +0900

    TODOコメントの解消

--- lib/features/repo_search/repo_search_repository.dart
+++ lib/features/repo_search/repo_search_repository.dart
@@ -6,9 +6,6 @@ import 'package:github_repo_searcher/features/repo_search/model/repo.dart';
 import '../api/query_param.dart';
 
 abstract class RepoSearchRepository {
-  // TODO(tsuruoka): データソースへの参照を記述する
-  // FutureProviderを並べる箱として機能する
-
   static final search = FutureProvider((ref) {
     return ref.watch(apiClient).get<Paging<Repo>>(
           path: '/search/repositories',
