a237a58def45ae9362643be1b34980cadfc6684f
tsuruo <tsuru.dev@gmail.com>
2022-07-22 14:22:34 +0900

    Remove comment

--- lib/app.dart
+++ lib/app.dart
@@ -9,7 +9,6 @@ import 'features/base/base_page.dart';
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
