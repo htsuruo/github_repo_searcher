38df3014bdca44c651c887de98807d9cfd0d2c26
tsuruo <tsuru.dev@gmail.com>
2022-07-22 14:12:34 +0900

    AsyncValueBuilderを作ってLoding/Errorを共通化

--- lib/features/repo_search/repo_search_page.dart
+++ lib/features/repo_search/repo_search_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:github_repo_searcher/features/pagination/model/paging.dart';
+import 'package:github_repo_searcher/features/repo_search/model/repo.dart';
 import 'package:github_repo_searcher/features/repo_search/repo_search_repository.dart';
 
 class RepoSearchPage extends ConsumerWidget {
@@ -7,14 +9,43 @@ class RepoSearchPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO(tsuruoka): implement
-    final repos = ref.watch(RepoSearchRepository.search);
-    return repos.when(
+    return AsyncValueBuilder<Paging<Repo>>(
+      value: ref.watch(RepoSearchRepository.search),
+      builder: (paging) => Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('totalCount: ${paging.totalCount}'),
+            Text('itemsLength: ${paging.items.length}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AsyncValueBuilder<T> extends StatelessWidget {
+  const AsyncValueBuilder({
+    super.key,
+    required this.value,
+    required this.builder,
+  });
+
+  final AsyncValue<T> value;
+  final Widget Function(T data) builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return value.when(
       loading: () => const Center(
         child: CircularProgressIndicator(),
       ),
-      error: (error, stackTrace) => Text(error.toString()),
-      data: (repos) => const Text('text'),
+      error: (error, stackTrace) => Center(
+        child: Text(
+          error.toString(),
+        ),
+      ),
+      data: builder,
     );
   }
 }
