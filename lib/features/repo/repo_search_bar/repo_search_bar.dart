0c691e0ed926cb67d92b472a54f7ff561a707917
tsuruo <tsuru.dev@gmail.com>
2022-07-24 12:16:51 +0900

    Form WidgetにGlobalFormKeyを設置しvalidation

--- lib/features/repo/repo_search_bar/repo_search_bar.dart
+++ lib/features/repo/repo_search_bar/repo_search_bar.dart
@@ -14,48 +14,50 @@ class RepoSearchBar extends ConsumerWidget {
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
     final controller = ref.watch(repoSearchBarController.notifier);
-    return TextFormField(
+    return Form(
       key: controller.formGlobalKey,
-      focusNode: controller.textFormFieldNode,
-      controller: controller.searchTextController,
-      autocorrect: false,
-      onFieldSubmitted: (_) {
-        controller.search();
-      },
-      validator: (value) => value == null || value.isEmpty
-          ? context.l10n.searchQueryEmptyValidation
-          : null,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      style: theme.textTheme.bodyMedium!.copyWith(
-        fontWeight: FontWeight.bold,
-      ),
-      decoration: InputDecoration(
-        contentPadding: const EdgeInsets.symmetric(
-          horizontal: 20,
-        ),
-        // constraints: const BoxConstraints(
-        //   maxHeight: 40,
-        // ),
-        hintText: context.l10n.searchRepoHint,
-        hintStyle: theme.textTheme.bodyMedium!.copyWith(
-          color: Colors.grey,
-        ),
-        errorStyle: theme.textTheme.bodySmall!.copyWith(
+      child: TextFormField(
+        focusNode: controller.textFormFieldNode,
+        controller: controller.searchTextController,
+        autocorrect: false,
+        onFieldSubmitted: (_) {
+          controller.search();
+        },
+        validator: (value) => value == null || value.isEmpty
+            ? context.l10n.searchQueryEmptyValidation
+            : null,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        style: theme.textTheme.bodyMedium!.copyWith(
           fontWeight: FontWeight.bold,
         ),
-        filled: true,
-        fillColor: colorScheme.primary.withOpacity(.1),
-        suffixIcon: IconButton(
-          focusNode: controller.iconButtonNode,
-          onPressed: () {
-            FocusScope.of(context).unfocus();
-            controller.search();
-          },
-          icon: const Icon(Icons.search),
-        ),
-        border: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.circular(40),
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          // constraints: const BoxConstraints(
+          //   maxHeight: 40,
+          // ),
+          hintText: context.l10n.searchRepoHint,
+          hintStyle: theme.textTheme.bodyMedium!.copyWith(
+            color: Colors.grey,
+          ),
+          errorStyle: theme.textTheme.bodySmall!.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+          filled: true,
+          fillColor: colorScheme.primary.withOpacity(.1),
+          suffixIcon: IconButton(
+            focusNode: controller.iconButtonNode,
+            onPressed: () {
+              FocusScope.of(context).unfocus();
+              controller.search();
+            },
+            icon: const Icon(Icons.search),
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(40),
+          ),
         ),
       ),
     );
